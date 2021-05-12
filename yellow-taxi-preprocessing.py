bb79942f89e3314c634894d2f75300b2f0d858e5
groth00 <busygroth@gmail.com>
2021-05-11 20:45:20 -0700

    join 2020 taxi
    Concatenate monthly data into one DataFrame, joined taxi zone information to identify pickup and dropoff locations

---
+++ yellow-taxi-preprocessing.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May 4 14:10:58 2021
+@author: groth
+"""
+
+import pandas as pd
+import glob
+
+files = glob.glob('yellow*.csv')
+dfs = []
+for f in sorted(files):
+    dfs.append(pd.read_csv(f, 
+    usecols=['VendorID', 'tpep_pickup_datetime', 'tpep_dropoff_datetime',
+              'passenger_count', 'trip_distance', 'PULocationID', 
+              'DOLocationID', 'total_amount'], 
+    parse_dates={'pickup_time': [1], 'dropoff_time': [2]}))
+df = pd.concat(dfs)
+df = df.dropna(axis = 0)
+
+df = df[(df['pickup_time'] > '2019-12-31') \
+          & (df['pickup_time'] < '2021-01-01')]
+
+zones = pd.read_csv('taxi_zone_lookup.csv')
+
+taxi = df.join(zones.set_index('LocationID'), 
+               on = 'PULocationID')
+taxi = taxi.join(zones.set_index('LocationID'), 
+                 on = 'DOLocationID', rsuffix = '_dropoff')
