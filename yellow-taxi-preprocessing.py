c312c1482a58c900b8bc3503fe64b85212391f33
groth00 <busygroth@gmail.com>
2021-05-11 22:39:13 -0700

    process jan yellow, jan for-hire, 2020 yellow
    code blocks for processing all of 2020 yellow taxi, january 2020 yellow taxi, january 2020 for-hire

--- yellow-taxi-preprocessing.py
+++ yellow-taxi-preprocessing.py
@@ -8,6 +8,8 @@ Created on Tue May 4 14:10:58 2021
 import pandas as pd
 import glob
 
+
+#### yellow taxi ALL OF 2020
 files = glob.glob('yellow*.csv')
 dfs = []
 for f in sorted(files):
@@ -25,6 +27,17 @@ df = df[(df['pickup_time'] > '2019-12-31') \
 df.to_csv('yellow_tripdata_2020.csv')
 
 
+#### for-hire vehicle JANUARY ONLY
 hv = pd.read_csv('fhvhv_tripdata_2020-01.csv')
 hv.SR_Flag = hv.SR_Flag.fillna(0)
 hv.to_csv('fhvhv_data_2020-01.csv')
+
+#### yellow taxi JANUARY ONLY
+jan = pd.read_csv('yellow_tripdata_2020-01.csv')
+jan = jan.drop(['store_and_fwd_flag', 'payment_type', 'fare_amount', 
+'extra', 'mta_tax', 'tip_amount', 
+'tolls_amount', 'improvement_surcharge', 'congestion_surcharge'], axis = 1)
+jan = jan.dropna(axis = 0)
+jan = jan[(jan['tpep_pickup_datetime'] > '2019-12-31') \
+         & (jan['tpep_pickup_datetime'] < '2020-02-01')]
+jan.to_csv('yellow_data_2020-01.csv')
